b8a6a8dd47d5a6cf3e8e9a5ee9dfe80568981d96
Максим Ф <maxfed@yandex.ru>
2022-06-23 18:14:00 +0300

    Сохранение в IDE

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int payment = 2000;
+        int overpayment = 20;
+        int miles = payment / overpayment;
+
+
+        System.out.println("Количество начисленных мили за купленный билет : " + miles);
+    }
+}
